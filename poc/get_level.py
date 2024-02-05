bd707252e089e824227cb907dfff5b1c91463c69
Moh Firdaus <mohammedfirdaus1404@gmail.com>
2024-02-05 21:59:32 +0700

    drop some column

--- poc/get_level.py
+++ poc/get_level.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def calculate_score_crit(hasil, column_name, numerator_column, denominator_column, weight):
+def calculate_score_crit(datalevel, column_name, numerator_column, denominator_column, weight):
     thresh = {
         'Revenue': [25.3657502341003],
         'Subscriber': [76107.3135000001],
@@ -8,8 +8,8 @@ def calculate_score_crit(hasil, column_name, numerator_column, denominator_colum
     }
     thresh_df = pd.DataFrame(thresh)
 
-    hasil[f'{column_name}_score_crit'] = (5 ** (hasil[numerator_column] / thresh_df[denominator_column])).apply(lambda x: 1 if x > 5 else (x - 1) / 4)
-    hasil[f'{column_name}_score_crit'] *= weight
+    datalevel[f'{column_name}_score_crit'] = (5 ** (datalevel[numerator_column] / thresh_df[denominator_column])).apply(lambda x: 1 if x > 5 else (x - 1) / 4)
+    datalevel[f'{column_name}_score_crit'] *= weight
 
 def get_level_category(value):
     if 0 <= value <= 40:
@@ -20,20 +20,20 @@ def get_level_category(value):
         return "BOD"
 
 def get_level(data):
-    hasil = pd.DataFrame(data)
+    datalevel = pd.DataFrame(data)
 
     # Example calls to calculate_score_crit function with respective parameters
-    calculate_score_crit(hasil, 'rev', 'revenue_hourly', 'Revenue', 40)
-    calculate_score_crit(hasil, 'subs', 'subscriber_4g_hourly', 'Subscriber', 55)
-    calculate_score_crit(hasil, 'site', 'site_id', 'Site', 5)
+    calculate_score_crit(datalevel, 'rev', 'revenue_hourly', 'Revenue', 40)
+    calculate_score_crit(datalevel, 'subs', 'subscriber_4g_hourly', 'Subscriber', 55)
+    calculate_score_crit(datalevel, 'site', 'site_id', 'Site', 5)
 
     # Example final_score_crit calculation
-    hasil['final_score_crit'] = hasil['rev_score_crit'] + hasil['subs_score_crit'] + hasil['site_score_crit']
+    datalevel['final_score_crit'] = datalevel['rev_score_crit'] + datalevel['subs_score_crit'] + datalevel['site_score_crit']
 
     # Example function to get level category
-    hasil['Level'] = hasil['final_score_crit'].apply(get_level_category)
+    datalevel['Level'] = datalevel['final_score_crit'].apply(get_level_category)
 
     # Example dropping unnecessary columns
-    result = hasil.drop(columns=['rev_score_crit', 'subs_score_crit', 'site_score_crit', 'No. Regional', 'Regional Name', 'Revenue Hourly', 'Subscriber 4G Hourly', 'Ticket Number','Site ID Uniq Down'])
+    result = datalevel.drop(columns=['rev_score_crit', 'subs_score_crit', 'site_score_crit', 'No. Regional', 'Regional Name', 'Revenue Hourly', 'Subscriber 4G Hourly', 'Ticket Number','Site ID Uniq Down'])
 
     return result
--- poc/get_severity.py
+++ poc/get_severity.py
@@ -1,6 +1,6 @@
-def calculate_score(merged_df, column_name, numerator_column, denominator_column, weight):
-    merged_df[f'{column_name}_score'] = (5 ** (merged_df[numerator_column] / merged_df[denominator_column])).apply(lambda x: 1 if x > 5 else (x - 1) / 4)
-    merged_df[f'{column_name}_score'] *= weight
+def calculate_score(dataseverity, column_name, numerator_column, denominator_column, weight):
+    dataseverity[f'{column_name}_score'] = (5 ** (dataseverity[numerator_column] / dataseverity[denominator_column])).apply(lambda x: 1 if x > 5 else (x - 1) / 4)
+    dataseverity[f'{column_name}_score'] *= weight
 
 def get_severity_category(value, regional_code):
     if regional_code in ['R03', 'R12']:
@@ -26,19 +26,19 @@ def get_severity_category(value, regional_code):
         else:
             return "Undefined"
 
-def get_severity(merged_df):
+def get_severity(dataseverity):
     # Use your functions here
-    calculate_score(merged_df, 'rev', 'revenue_hourly', 'Revenue Hourly', 0.4)
-    calculate_score(merged_df, 'subs', 'subscriber_4g_hourly', 'Subscriber 4G Hourly', 0.55)
-    calculate_score(merged_df, 'site', 'site_id', 'Site ID Uniq Down', 0.05)
+    calculate_score(dataseverity, 'rev', 'revenue_hourly', 'Revenue Hourly', 0.4)
+    calculate_score(dataseverity, 'subs', 'subscriber_4g_hourly', 'Subscriber 4G Hourly', 0.55)
+    calculate_score(dataseverity, 'site', 'site_id', 'Site ID Uniq Down', 0.05)
 
     # Calculate the final score
-    merged_df['final_score'] = merged_df['rev_score'] + merged_df['subs_score'] + merged_df['site_score']
+    dataseverity['final_score'] = dataseverity['rev_score'] + dataseverity['subs_score'] + dataseverity['site_score']
 
     # Apply severity category to final_score and add 'Severity' column to the dataframe
-    merged_df['Severity'] = merged_df.apply(lambda row: get_severity_category(row['final_score'], row['No. Regional']), axis=1)
+    dataseverity['Severity'] = dataseverity.apply(lambda row: get_severity_category(row['final_score'], row['No. Regional']), axis=1)
 
     # Drop unnecessary columns from the merged dataframe
-    hasil = merged_df.drop(columns=['No. Regional', 'Regional Name', 'Revenue Hourly', 'Subscriber 4G Hourly', 'Ticket Number','Site ID Uniq Down'])
+    hasil = dataseverity.drop(columns=['No. Regional', 'Regional Name', 'Revenue Hourly', 'Subscriber 4G Hourly', 'Ticket Number','Site ID Uniq Down'])
     
     return hasil
