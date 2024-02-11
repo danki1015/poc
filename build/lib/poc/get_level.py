f64b378a4edccd3306c18283f4129e9c3538c7f4
Moh Firdaus <mohammedfirdaus1404@gmail.com>
2024-02-12 00:14:07 +0700

    updatee

--- build/lib/poc/get_level.py
+++ build/lib/poc/get_level.py
@@ -1,6 +1,33 @@
 import pandas as pd
+from .get_severity import get_severity
 
 def calculate_score_crit(datalevel, column_name, numerator_column, denominator_column, weight):
+
+    datalevel[f'{column_name}_score_crit'] = (5 ** (datalevel[numerator_column] / datalevel[denominator_column])).apply(lambda x: 1 if x > 5 else (x - 1) / 4)
+    datalevel[f'{column_name}_score_crit'] *= weight
+
+def get_level_category(value, severity):
+    if severity == 'Critical':
+        if 0 <= value <= 40:
+            return "GM"
+        elif 40 < value <= 98:
+            return "VP"
+        else:
+            return "BOD"
+    elif severity == 'Major':
+        return "GM"
+    elif severity == 'Minor':
+        return "Manager"
+    elif severity == 'Low':
+        return "SPV"
+    else:
+        return "Undefined" 
+
+def get_level(datalevel):
+    # Add 'Severity' column if not already present
+    if 'Severity' or 'final_score' not in datalevel.columns:
+        datalevel['Severity'] = get_severity(datalevel)['Severity']
+        datalevel['final_score'] = get_severity(datalevel)['final_score']
 
     thresh = {
         'Revenue': [25.3657502341003],
@@ -9,21 +36,8 @@ def calculate_score_crit(datalevel, column_name, numerator_column, denominator_c
     }
 
     # Tambahkan nilai dari threshold ke DataFrame Anda
-    datalevel = datalevel.assign(**thresh)
-
-    datalevel[f'{column_name}_score_crit'] = (5 ** (datalevel[numerator_column] / thresh[denominator_column])).apply(lambda x: 1 if x > 5 else (x - 1) / 4)
-    datalevel[f'{column_name}_score_crit'] *= weight
-
-def get_level_category(value):
-    if 0 <= value <= 40:
-        return "GM"
-    elif 40 < value <= 98:
-        return "VP"
-    else:
-        return "BOD"
-
-def get_level(data):
-    datalevel = pd.DataFrame(data)
+    for col, values in thresh.items():
+        datalevel[col] = values[0]
 
     # Example calls to calculate_score_crit function with respective parameters
     calculate_score_crit(datalevel, 'rev', 'revenue_hourly', 'Revenue', 40)
@@ -34,9 +48,13 @@ def get_level(data):
     datalevel['final_score_crit'] = datalevel['rev_score_crit'] + datalevel['subs_score_crit'] + datalevel['site_score_crit']
 
     # Example function to get level category
-    datalevel['Level'] = datalevel['final_score_crit'].apply(get_level_category)
+    datalevel['Level'] = datalevel.apply(lambda row: get_level_category(row['final_score_crit'], row['Severity']), axis=1)
 
-    # Example dropping unnecessary columns
-    result = datalevel.drop(columns=['rev_score_crit', 'subs_score_crit', 'site_score_crit', 'No. Regional', 'Regional Name', 'Revenue Hourly', 'Subscriber 4G Hourly', 'Ticket Number','Site ID Uniq Down'])
+    selected_columns = ['Ticket ID', 'regional', 'Start Time(Create TT_alarm_start_time)',
+                    'Severity(Create TT_severity)', 'revenue_hourly', 'site_id',
+                    'subscriber_4g_hourly', 'final_score', 'Severity', 'final_score_crit', 'Level']
+
+    result = datalevel[selected_columns]
+    result.loc[result['Severity'] != 'Critical', 'final_score_crit'] = 0
 
     return result
--- build/lib/poc/get_severity.py
+++ build/lib/poc/get_severity.py
@@ -1,3 +1,6 @@
+from .merge import merge
+import pandas as pd
+
 def calculate_score(dataseverity, column_name, numerator_column, denominator_column, weight):
     dataseverity[f'{column_name}_score'] = (5 ** (dataseverity[numerator_column] / dataseverity[denominator_column])).apply(lambda x: 1 if x > 5 else (x - 1) / 4)
     dataseverity[f'{column_name}_score'] *= weight
@@ -27,6 +30,23 @@ def get_severity_category(value, regional_code):
             return "Undefined"
 
 def get_severity(dataseverity):
+    indexing = {
+    'No. Regional': ['R01', 'R02', 'R03', 'R04', 'R05',
+                 'R06', 'R07', 'R08', 'R09', 'R10',
+                 'R11', 'R12'],
+    'Regional Name': ['SUMBAGUT', 'SUMBAGSEL', 'INNER JABOTABEK', 'WEST JAVA', 'CENTRAL JAVA & DIY',
+                 'EAST JAVA', 'BALI NUSRA', 'KALIMANTAN', 'SULAWESI', 'SUMBAGTENG',
+                 'PUMA', 'OUTER JABOTABEK'],
+    'Revenue Hourly': [1517950, 1170221, 477582, 543003, 575347, 551735, 573193, 1344308, 1208702, 1419904, 809735, 502344],
+    'Subscriber 4G Hourly': [3856, 3216, 1633.4, 1847.95, 1804.4, 1564, 1543.8, 2893, 3265.6, 3642.5, 2142.5, 1616.4],
+    'Site ID Uniq Down': [8, 6, 2, 4, 3, 3, 3, 6, 7, 7, 4, 3],
+    }
+    # Create DataFrame
+    indexing = pd.DataFrame(indexing)
+    indexing['Revenue Hourly'] = indexing['Revenue Hourly']/1000000 
+
+    dataseverity = merge(dataseverity, indexing)
+
     # Use your functions here
     calculate_score(dataseverity, 'rev', 'revenue_hourly', 'Revenue Hourly', 0.4)
     calculate_score(dataseverity, 'subs', 'subscriber_4g_hourly', 'Subscriber 4G Hourly', 0.55)
@@ -38,7 +58,11 @@ def get_severity(dataseverity):
     # Apply severity category to final_score and add 'Severity' column to the dataframe
     dataseverity['Severity'] = dataseverity.apply(lambda row: get_severity_category(row['final_score'], row['No. Regional']), axis=1)
 
-    # Drop unnecessary columns from the merged dataframe
-    hasil = dataseverity.drop(columns=['No. Regional', 'Regional Name', 'Revenue Hourly', 'Subscriber 4G Hourly', 'Ticket Number','Site ID Uniq Down'])
-    
+    selected_columns = ['Ticket ID', 'regional', 'Start Time(Create TT_alarm_start_time)',
+                    'Severity(Create TT_severity)', 'revenue_hourly', 'site_id',
+                    'subscriber_4g_hourly', 'rev_score', 'subs_score', 'site_score',
+                    'final_score', 'Severity']
+
+    hasil = dataseverity[selected_columns]
+
     return hasil
